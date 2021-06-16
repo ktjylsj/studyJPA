2ed72bccb43ccc3e1339ecdad0e22ebcca7cbc08
taejink <tjkimts@naver.com>
2021-06-16 21:39:59 +0900

    20210616 - 엔티티 매핑
    객체와 테이블 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,8 +2,10 @@ package hellojpa;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+// @Table(name = "MBR")    // 테이블명을 바꾸고 싶을 때 사용
 public class Member {
     @Id
     private Long id;
